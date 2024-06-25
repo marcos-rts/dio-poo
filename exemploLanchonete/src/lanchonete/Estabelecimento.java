aa134d8b20a9acf20e2b269014b41fc6e795aa01
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-24 22:50:38 -0300

    Mudança de visibilidade das classes

--- exemploLanchonete/src/lanchonete/Estabelecimento.java
+++ exemploLanchonete/src/lanchonete/Estabelecimento.java
@@ -9,32 +9,32 @@ public class Estabelecimento {
 	public static void main(String[] args) {
 		Cozinheiro cozinheiro = new Cozinheiro();
 		//ações que não precisam estarem disponíveis para toda a aplicação
-		cozinheiro.lavarIngredientes();
-		cozinheiro.baterVitaminaLiquidificador();
-		cozinheiro.selecionarIngredientesVitamina();
-		cozinheiro.prepararLanche();
-		cozinheiro.prepararVitamina();
-		cozinheiro.prepararVitamina();
+		// cozinheiro.lavarIngredientes();
+		// cozinheiro.baterVitaminaLiquidificador();
+		// cozinheiro.selecionarIngredientesVitamina();
+		// cozinheiro.prepararLanche();
+		// cozinheiro.prepararVitamina();
+		// cozinheiro.prepararVitamina();
 		
 		//ações que estabelecimento precisa ter ciência
 		cozinheiro.adicionarSucoNoBalcao();;
 		cozinheiro.adicionarLancheNoBalcao();
 		cozinheiro.adicionarComboNoBalcao();
 		
-		Almoxarife almoxarife = new Almoxarife();
+		//Almoxarife almoxarife = new Almoxarife();
 		//ações que não precisam estarem disponíveis para toda a aplicação
-		almoxarife.controlarEntrada();
-		almoxarife.controlarSaida();
+		// almoxarife.controlarEntrada();
+		// almoxarife.controlarSaida();
 		//ação que somente o seu pacote cozinha precisa conhecer (default)
-		almoxarife.entregarIngredientes();
-		almoxarife.trocarGas();
+		//almoxarife.entregarIngredientes();
+		//almoxarife.trocarGas();
 		
 		Atendente atendente = new Atendente();
-		atendente.pegarLancheCozinha();
+		//atendente.pegarLancheCozinha();
 		atendente.receberPagamento();
 		atendente.servindoMesa();
 		//ação que somente o seu pacote cozinha precisa conhecer (default)
-		atendente.trocarGas();
+		//atendente.trocarGas();
 		
 		Cliente cliente = new Cliente();
 		cliente.escolherLanche();
@@ -43,14 +43,14 @@ public class Estabelecimento {
 		
 		//não deveria, mas o estabelecimento
 		//ainda não definiu normas de atendimento
-		cliente.pegarPedidoBalcao();
+		//cliente.pegarPedidoBalcao();
 		
 		//esta ação é muito sigilosa, qua tal ser privada ?
-		cliente.consultarSaldoAplicativo();
+		//cliente.consultarSaldoAplicativo();
 		
 		//já pensou os clientes ouvindo que o gás acabou ?
-		cozinheiro.pedirParaTrocarGas(atendente);
-		cozinheiro.pedirParaTrocarGas(almoxarife);
+		//cozinheiro.pedirParaTrocarGas(atendente);
+		//cozinheiro.pedirParaTrocarGas(almoxarife);
 		
 	}
 }
